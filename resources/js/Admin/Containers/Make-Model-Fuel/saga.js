c85c04baa1c976c330f2503a5be7423078ad398a
Sourav_ <you@example.com>
2021-04-01 15:28:47 +0530

    feat Product price

--- resources/js/Admin/Containers/Make-Model-Fuel/saga.js
+++ resources/js/Admin/Containers/Make-Model-Fuel/saga.js
@@ -84,6 +84,10 @@ function* modelData() {
         if (data) {
             yield put(modelDataSuccess(data));
         }
+        else{
+            message.error('System Error');
+            console.log('wrong')
+        }
     } catch (error) {
         message.error(error);
     }
