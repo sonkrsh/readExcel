ce95d1405a512af9fa154fb7519cbb233e6da7e7
sourav <sourav.singh@emoha.com>
2021-07-23 00:17:04 +0530

    fix:master update

--- automobileCrunch/resources/js/Index.js
+++ automobileCrunch/resources/js/Index.js
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Homepage from "./User/Containers/Homepage";
-import Products from "./User/Containers/Products";
-import CheckOut from "./User/Containers/CheckOut";
-import AdminRoute from "./Admin/AdminRoute";
-import Navbar from "./User/Components/navBar";
+
+import AdminLoginPage from "./Admin/Containers/AdminLoginPage";
+import AdminLayout from "./Admin/Containers/AdminLayout";
+
+import UserRoute from "./User/UserRoute";
+
 import { Provider } from "react-redux";
 import store from "./Store/store";
 import "./style.css";
 
+const PrivateRoute = lazy(() => import("./Admin/utils/PrivateRoute"));
+const ProtectedRoute = lazy(() => import("./Admin/utils/ProtectedRoute"));
 function Index(props) {
     return (
         <Provider store={store}>
             <div className="container-fluid">
-                <Navbar />
                 <div className="pageContent">
-                    <Switch>
-                        <Route exact path="/" component={Homepage} />
-                        <Route
-                            exact
-                            path="/:makeName/:modelName/:fuelName/:locationName?"
-                            component={Products}
-                        />
-                        <Route exact path="/checkout" component={CheckOut} />
-                        <Route component={AdminRoute} />
-                    </Switch>
+                    <Suspense fallback={<div>Loading.....</div>}>
+                        <Switch>
+                            <ProtectedRoute
+                                exact
+                                path="/admin-login"
+                                component={AdminLoginPage}
+                            />
+                            <PrivateRoute
+                                //exact
+                                path="/Dashboard"
+                                component={AdminLayout}
+                            />
+
+                            <Route component={UserRoute} />
+                        </Switch>
+                    </Suspense>
                 </div>
             </div>
         </Provider>
