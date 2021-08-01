108725dd929fb4c1f0edf2bb209a5b809fc56176
sourav <sourav.singh@emoha.com>
2021-08-01 23:43:12 +0530

    made util makeModel Fuel

--- resources/js/Admin/Containers/AllocateGlass/index.js
+++ resources/js/Admin/Containers/AllocateGlass/index.js
@@ -2,10 +2,10 @@ import React, { useEffect } from "react";
 import AllocateBattery from "../../Components/allocateBattery";
 import { modelData, fuelData, locationData } from "../Make-Model-Fuel/action";
 import { companyModelData } from "../Battery/action";
-import { useDispatch,useSelector } from "react-redux";
-import { formSubmit,getProductData } from "./actions";
+import { useDispatch, useSelector } from "react-redux";
+import { formSubmit, getProductData } from "./actions";
 import { Tabs } from "antd";
-import GetBattery from '../../Components/allocateBattery/getBattery'
+import AllocateGlass from "../../Components/AllocateGlass";
 
 function index() {
     const dispatch = useDispatch();
@@ -17,22 +17,13 @@ function index() {
         dispatch(fuelData());
         dispatch(locationData());
         dispatch(companyModelData());
-        dispatch(getProductData())
     }, []);
 
     return (
         <div>
             <Tabs defaultActiveKey="1" tabPosition={"top"}>
-                <TabPane tab={"Dashboard"} key={1}>
-                <GetBattery productData= {reducerProps.productData}/>
-
-                </TabPane>
-                <TabPane tab={"Add Products"} key={2}>
-                    <AllocateBattery
-                        onSubmit={(event) => {
-                            dispatch(formSubmit(event));
-                        }}
-                    />
+                <TabPane tab={"Allocate Glass"} key={1}>
+                    <AllocateGlass />
                 </TabPane>
             </Tabs>
         </div>
