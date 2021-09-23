e76a698ce21c24cca0c1a871c09500b3a18dc0bb
sourav <sourav.singh@emoha.com>
2021-09-23 19:44:07 +0530

    fix: clear and make mail functionality

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>AutoMobile</title>
+        <title>Read Excel</title>
 
     </head>
         <body>
